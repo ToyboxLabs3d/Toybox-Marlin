3c351b7ae505d2d746f0af650f38084bb5c54e40
Alex Haynes <alex@toybox.com>
2025-05-28 16:55:43 -0700

    different build numbers for Alpha3 and Charlie

--- Marlin/src/gcode/toybox/M10002.cpp
+++ Marlin/src/gcode/toybox/M10002.cpp
@@ -1,8 +1,15 @@
 #include "../gcode.h"
 
-
-#define BUILD_NUMBER 1
-#define VERSION_STRING "1.0.0"
+#ifdef ENV_ALPHA3
+    #define BUILD_NUMBER 1
+    #define VERSION_STRING "1.0.0"
+#elif defined(ENV_CHARLIE)
+    #define BUILD_NUMBER 100
+    #define VERSION_STRING "1.0.0"
+#else
+    #define BUILD_NUMBER (-1)
+    #define VERSION_STRING "Unknown"
+#endif
 
 void GcodeSuite::M10002(){
   SERIAL_ECHO_MSG("Toybox-marlin BUILD: ", BUILD_NUMBER, " VERSION: ", VERSION_STRING);
