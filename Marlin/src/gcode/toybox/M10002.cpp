da4fd0d09b34da81d28e121e17ea2d72ad242133
Alex Haynes <alex@toybox.com>
2025-05-28 16:38:31 -0700

    fix capitalization

--- Marlin/src/gcode/toybox/M10002.cpp
+++ Marlin/src/gcode/toybox/M10002.cpp
@@ -5,6 +5,5 @@
 #define VERSION_STRING "1.0.0"
 
 void GcodeSuite::M10002(){
-  SERIAL_ECHO_MSG("Toybox-marlin BUILD: ", BUILD_NUMBER, " Version: ", VERSION_STRING);
-//   SERIAL_ECHO_MSG("Toybox-marlin BUILD");
+  SERIAL_ECHO_MSG("Toybox-marlin BUILD: ", BUILD_NUMBER, " VERSION: ", VERSION_STRING);
 }
