6e13eb4e63936d2074ea6e322b4ef58bf7ba4a9d
ToyboxZach <64285391+ToyboxZach@users.noreply.github.com>
2025-06-24 20:27:10 -0700

    More charlie settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -625,9 +625,9 @@
 #endif
 
 #if TEMP_SENSOR_BED
-  #define TEMP_BED_RESIDENCY_TIME     10  // (seconds) Time to wait for bed to "settle" in M190
-  #define TEMP_BED_WINDOW              1  // (°C) Temperature proximity for the "temperature reached" timer
-  #define TEMP_BED_HYSTERESIS          3  // (°C) Temperature proximity considered "close enough" to the target
+  #define TEMP_BED_RESIDENCY_TIME      0  // (seconds) Time to wait for bed to "settle" in M190
+  #define TEMP_BED_WINDOW              5  // (°C) Temperature proximity for the "temperature reached" timer
+  #define TEMP_BED_HYSTERESIS          5  // (°C) Temperature proximity considered "close enough" to the target
 #endif
 
 #if TEMP_SENSOR_CHAMBER
@@ -1336,7 +1336,7 @@
  */
 
 #ifdef ENV_CHARLIE
-  #define DEFAULT_MAX_FEEDRATE          { 350, 350, 5, 30 }
+  #define DEFAULT_MAX_FEEDRATE          { 350, 350, 10, 30 }
 #elif defined(ENV_ALPHA3)
   #define DEFAULT_MAX_FEEDRATE          { 300, 300, 10, 100 }
 #else 
@@ -1688,7 +1688,7 @@
 #ifdef ENV_ALPHA3
   #define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
 #elif defined(ENV_CHARLIE)
-  #define NOZZLE_TO_PROBE_OFFSET { 0, 25, 0 }
+  #define NOZZLE_TO_PROBE_OFFSET { 0, 25, -0.1 }
 #endif
 
 
@@ -1704,18 +1704,18 @@
 #ifdef ENV_ALPHA3
   #define PROBING_MARGIN 10
 #elif defined(ENV_CHARLIE)
-  #define PROBING_MARGIN 30
+  #define PROBING_MARGIN 10
 #endif
 
 // X and Y axis travel speed between probes.
 // Leave undefined to use the average of the current XY homing feedrate.
-#define XY_PROBE_FEEDRATE    (133*60) // (mm/min)
+#define XY_PROBE_FEEDRATE    (350*60) // (mm/min)
 
 // Feedrate for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST  (4*60) // (mm/min)
+#define Z_PROBE_FEEDRATE_FAST  (20*60) // (mm/min)
 
 // Feedrate for the "accurate" probe of each point
-#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2) // (mm/min)
+#define Z_PROBE_FEEDRATE_SLOW (4*60) // (mm/min)
 
 /**
  * Probe Activation Switch
@@ -1957,7 +1957,7 @@
 
 #ifdef ENV_CHARLIE
   #define X_BED_SIZE 190
-  #define Y_BED_SIZE 190
+  #define Y_BED_SIZE 185
 #elif defined(ENV_ALPHA3)
   #define X_BED_SIZE 80
   #define Y_BED_SIZE 80
@@ -1968,8 +1968,8 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#ifdef ENV_CHARLIER
-#define Y_MIN_POS -8
+#ifdef ENV_CHARLIE
+#define Y_MIN_POS 0
 #else
 #define Y_MIN_POS 0
 #endif
@@ -2104,7 +2104,7 @@
   // Commands to execute on filament runout.
   // With multiple runout sensors use the %c placeholder for the current tool in commands (e.g., "M600 T%c")
   // NOTE: After 'M412 H1' the host handles filament runout and this script does not apply.
-  #define FILAMENT_RUNOUT_SCRIPT "M118 filament_out\nM600"
+  #define FILAMENT_RUNOUT_SCRIPT "M118 filament_out\n"
 
   // After a runout is detected, continue printing this length of filament
   // before executing the runout script. Useful for a sensor at the end of
@@ -2255,7 +2255,7 @@
    */
   #define ENABLE_LEVELING_FADE_HEIGHT
   #if ENABLED(ENABLE_LEVELING_FADE_HEIGHT)
-    #define DEFAULT_LEVELING_FADE_HEIGHT 10.0 // (mm) Default fade height.
+    #define DEFAULT_LEVELING_FADE_HEIGHT 2.0 // (mm) Default fade height.
   #endif
 
   /**
@@ -2285,7 +2285,7 @@
 #if ANY(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
@@ -2295,7 +2295,7 @@
 
     // Beyond the probed grid, continue the implied tilt?
     // Default is to maintain the height of the nearest edge.
-    //#define EXTRAPOLATE_BEYOND_GRID
+    #define EXTRAPOLATE_BEYOND_GRID
 
     //
     // Subdivision of the grid by Catmull-Rom method.
@@ -2447,8 +2447,11 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
+#ifdef ENV_CHARLIE
+#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (10*60) } // X, Y, Z
+#else
 #define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60) }
-
+#endif
 // Edit homing feedrates with M210 and MarlinUI menu items
 //#define EDITABLE_HOMING_FEEDRATE
 
@@ -2597,7 +2600,7 @@
 #endif
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z_raise }
-  #define NOZZLE_PARK_POINT { (X_MIN_POS + 11), (Y_MIN_POS + 1), 20 }
+  #define NOZZLE_PARK_POINT { (X_MIN_POS + 1), (Y_MIN_POS + 1), 20 }
   #define NOZZLE_PARK_MOVE          4   // Park motion: 0 = XY Move, 1 = X Only, 2 = Y Only, 3 = X before Y, 4 = Y before X
   #define NOZZLE_PARK_Z_RAISE_MIN   2   // (mm) Always raise Z by at least this distance
   #define NOZZLE_PARK_XY_FEEDRATE 100   // (mm/s) X and Y axes feedrate (also used for delta Z axis)
@@ -2642,17 +2645,18 @@
  *
  *   Caveats: The ending Z should be the same as starting Z.
  */
-//#define NOZZLE_CLEAN_FEATURE
-
+#ifdef ENV_CHARLIE
+#define NOZZLE_CLEAN_FEATURE
+#endif
 #if ENABLED(NOZZLE_CLEAN_FEATURE)
   #define NOZZLE_CLEAN_PATTERN_LINE     // Provide 'G12 P0' - a simple linear cleaning pattern
-  #define NOZZLE_CLEAN_PATTERN_ZIGZAG   // Provide 'G12 P1' - a zigzag cleaning pattern
-  #define NOZZLE_CLEAN_PATTERN_CIRCLE   // Provide 'G12 P2' - a circular cleaning pattern
+//  #define NOZZLE_CLEAN_PATTERN_ZIGZAG   // Provide 'G12 P1' - a zigzag cleaning pattern
+  //#define NOZZLE_CLEAN_PATTERN_CIRCLE   // Provide 'G12 P2' - a circular cleaning pattern
 
   // Default pattern to use when 'P' is not provided to G12. One of the enabled options above.
   #define NOZZLE_CLEAN_DEFAULT_PATTERN 0
 
-  #define NOZZLE_CLEAN_STROKES     12   // Default number of pattern repetitions
+  #define NOZZLE_CLEAN_STROKES     5   // Default number of pattern repetitions
 
   #if ENABLED(NOZZLE_CLEAN_PATTERN_ZIGZAG)
     #define NOZZLE_CLEAN_TRIANGLES  3   // Default number of triangles
@@ -2660,9 +2664,9 @@
 
   // Specify positions for each tool as { { X, Y, Z }, { X, Y, Z } }
   // Dual hotend system may use { {  -20, (Y_BED_SIZE / 2), (Z_MIN_POS + 1) },  {  420, (Y_BED_SIZE / 2), (Z_MIN_POS + 1) }}
-  #define NOZZLE_CLEAN_START_POINT { {  30, 30, (Z_MIN_POS + 1) } }
-  #define NOZZLE_CLEAN_END_POINT   { { 100, 60, (Z_MIN_POS + 1) } }
-
+  #define NOZZLE_CLEAN_START_POINT { {  150, 185, (Z_MIN_POS + 1) } }
+  #define NOZZLE_CLEAN_END_POINT   { { 185, 185, (Z_MIN_POS + 1) } }
+  #define CLEAN_FEEDRATE 350
   #if ENABLED(NOZZLE_CLEAN_PATTERN_CIRCLE)
     #define NOZZLE_CLEAN_CIRCLE_RADIUS 6.5                      // (mm) Circular pattern radius
     #define NOZZLE_CLEAN_CIRCLE_FN 10                           // Circular pattern circle number of segments
@@ -2673,17 +2677,17 @@
   #define NOZZLE_CLEAN_GOBACK
 
   // For a purge/clean station that's always at the gantry height (thus no Z move)
-  //#define NOZZLE_CLEAN_NO_Z
+  #define NOZZLE_CLEAN_NO_Z
 
   // For a purge/clean station mounted on the X axis
-  //#define NOZZLE_CLEAN_NO_Y
+  #define NOZZLE_CLEAN_NO_Y
 
   // Require a minimum hotend temperature for cleaning
   #define NOZZLE_CLEAN_MIN_TEMP 170
   //#define NOZZLE_CLEAN_HEATUP       // Heat up the nozzle instead of skipping wipe
 
   // Explicit wipe G-code script applies to a G12 with no arguments.
-  //#define WIPE_SEQUENCE_COMMANDS "G1 X-17 Y25 Z10 F4000\nG1 Z1\nM114\nG1 X-17 Y25\nG1 X-17 Y95\nG1 X-17 Y25\nG1 X-17 Y95\nG1 X-17 Y25\nG1 X-17 Y95\nG1 X-17 Y25\nG1 X-17 Y95\nG1 X-17 Y25\nG1 X-17 Y95\nG1 X-17 Y25\nG1 X-17 Y95\nG1 Z15\nM400\nG0 X-10.0 Y-9.0"
+  //#define WIPE_SEQUENCE_COMMANDS "G90\nG1 F21000 X150 Y180 \nG1 X180\nG1 X150\nG1 X180\nG1 X150\nG1 X180\nG1 X150\nG1 X180\nG1 X150\nG1 X180\n"
 
 #endif
 
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -954,7 +954,7 @@
 //#define HOMING_BUMP_MM      { 5, 5, 2 }       // (linear=mm, rotational=°) Backoff from endstops after first bump
 #define HOMING_BUMP_DIVISOR { 2, 2, 4 }       // Re-Bump Speed Divisor (Divides the Homing Feedrate)
 
-//#define HOMING_BACKOFF_POST_MM { 2, 2, 2 }  // (linear=mm, rotational=°) Backoff from endstops after homing
+#define HOMING_BACKOFF_POST_MM { 5, 0, 0 }  // (linear=mm, rotational=°) Backoff from endstops after homing
 //#define XY_COUNTERPART_BACKOFF_MM 0         // (mm) Backoff X after homing Y, and vice-versa
 #ifdef ENV_CHARLIE
 #define HOME_Y_BEFORE_X 
@@ -2428,10 +2428,10 @@
  */
 #if PROBE_SELECTED && !IS_KINEMATIC
   #ifdef ENV_CHARLIE
-    #define PROBING_MARGIN_LEFT PROBING_MARGIN
-    #define PROBING_MARGIN_RIGHT PROBING_MARGIN
-    #define PROBING_MARGIN_FRONT PROBING_MARGIN
-    #define PROBING_MARGIN_BACK PROBING_MARGIN
+    #define PROBING_MARGIN_LEFT 15
+    #define PROBING_MARGIN_RIGHT 15
+    #define PROBING_MARGIN_FRONT 15
+    #define PROBING_MARGIN_BACK 15
 #endif
 #endif
 
