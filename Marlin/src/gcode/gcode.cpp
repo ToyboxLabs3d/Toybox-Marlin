3462efc7388525c0d86697de4692ea80dd545a3f
Alex Haynes <alex@toybox.com>
2025-08-21 19:43:20 -0700

    comment

--- Marlin/src/gcode/gcode.cpp
+++ Marlin/src/gcode/gcode.cpp
@@ -244,6 +244,7 @@ void GcodeSuite::get_destination_from_command() {
 }
 
 /**
+ * ToyboxAlex: G4 syncronizes; This is misleading.
  * Dwell waits immediately. It does not synchronize. Use M400 instead of G4
  */
 void GcodeSuite::dwell(millis_t time) {
