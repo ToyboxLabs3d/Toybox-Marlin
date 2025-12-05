457dd3b7496a697541ef9b8b64b9fb23a43f9c76
Alex Haynes <alex@toybox.com>
2025-12-05 10:07:29 -0800

    futher reduce mintemp

--- Marlin/src/gcode/toybox/M10002.cpp
+++ Marlin/src/gcode/toybox/M10002.cpp
@@ -1,7 +1,7 @@
 #include "../gcode.h"
 
 #ifdef ENV_ALPHA3
-    #define BUILD_NUMBER 7
+    #define BUILD_NUMBER 9
     #define VERSION_STRING "1.0.1"
 #elif defined(ENV_CHARLIE)
     #define BUILD_NUMBER 101
