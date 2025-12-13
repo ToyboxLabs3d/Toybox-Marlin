50e566a14cb46cd29a2be24f2dc56b970ba08620
Alex Haynes <alex@toybox.com>
2025-12-13 08:21:46 -0800

    Disable M0 and M1

--- Marlin/src/feature/e_parser.cpp
+++ Marlin/src/feature/e_parser.cpp
@@ -52,7 +52,10 @@ bool EmergencyParser::killed_by_M112, // = false
 EmergencyParser emergency_parser;
 
 // External references
-extern bool wait_for_user, wait_for_heatup;
+#if HAS_RESUME_CONTINUE
+extern bool wait_for_user;
+#endif
+extern bool wait_for_heatup;
 
 #if ENABLED(EP_BABYSTEPPING)
   #include "babystep.h"
@@ -224,7 +227,12 @@ void EmergencyParser::update(EmergencyParser::State &state, const uint8_t c) {
     default:
       if (ISEOL(c)) {
         if (enabled) switch (state.state_machine_state) {
-          case EP_M108: wait_for_user = wait_for_heatup = false; break;
+          case EP_M108:
+          #if HAS_PAUSE_RESUME
+           wait_for_user = false;
+          #endif
+            wait_for_heatup = false; 
+            break;
           case EP_M112: killed_by_M112 = true; break;
           case EP_M410: quickstop_by_M410 = true; break;
           #if ENABLED(EP_BABYSTEPPING)
