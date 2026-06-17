a817e251d66acc21b121d460803904b3be147121
Alex Haynes <alex@toybox.com>
2026-06-17 14:33:12 -0700

    bump build number

--- Marlin/src/gcode/toybox/M10002.cpp
+++ Marlin/src/gcode/toybox/M10002.cpp
@@ -4,7 +4,7 @@
     #define BUILD_NUMBER 10
     #define VERSION_STRING "1.0.1"
 #elif defined(ENV_ALPHA4)
-    #define BUILD_NUMBER 210
+    #define BUILD_NUMBER 212
     #define VERSION_STRING "1.0.0"
 #elif defined(ENV_CHARLIE)
     #define BUILD_NUMBER 101
